3e9bdd33b3f164471a8cae3a309c1dd795fd15a0
Atsuya Takagi <atsuya.takagi@gmail.com>
2010-08-28 15:16:14 -0700

    Added something.

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+
 var staticResource = require('./libraries/static-resource');
+var mustache = require('./libraries/mustache');
 
 var handler = staticResource.createHandler(fs.realpathSync('./static'));
+//var mustachedPages = new Array('/index.html');
 
 var server = http.createServer(function(request, response) {
     var path = url.parse(request.url).pathname;
     if(path == '/') {
         path = '/coming.html';
     }
-    
-    if(!handler.handle(path, request, response)) {
-        response.writeHead(404);
-        response.write('404');
-        response.end();
+
+    /*
+    if(mustachedPages[path]) {
+        
+    } else {
+    */
+        if(!handler.handle(path, request, response)) {
+            response.writeHead(404);
+            response.write('404');
+            response.end();
+        }
+    /*
     }
+    */
 });
-server.listen(8080);
+server.listen(80);
